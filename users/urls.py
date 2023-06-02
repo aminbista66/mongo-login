81e83a2d2bcdd1adfdd87160ed50a0517889d2dd
Amin Bista <aminbista6666@gmail.com>
2023-06-02 21:06:55 +0545

    added protected template for testing purpose

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.LoginView.as_view(), name='login'),
-    path('register/', views.RegisterView.as_view(), name='register')
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('protected/', views.ProtectedView.as_view(), name='protected'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,11 +3,14 @@ from django.views import generic
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from .helper import create_user_object
-from .mongodb import get_db, user_collection, user
+from .mongodb import user
 
 class LoginView(generic.View):
     def get(self, *args, **kwargs):
         return render(self.request, 'users/login.html', {})
+
+    def post(self, *args, **kwargs):
+        return HttpResponseRedirect(reverse_lazy('users:protected'))
 
 class RegisterView(generic.View):
     def get(self, *args, **kwargs):
@@ -22,10 +25,15 @@ class RegisterView(generic.View):
 
         for i in post_object.keys():
             post_object[i] = self.request.POST.get(str(i))
+
+        # contains hashed password
         user_object = create_user_object(post_object)
-        print(user_object)
+
         try:
             user.insert_one(user_object)
         except Exception as e:
             raise e
         return HttpResponseRedirect(reverse_lazy('users:login'))
+
+class ProtectedView(generic.TemplateView):
+    template_name = "protected/index.html"
